be43184409765ffe8c80f83bb91cb1cb5f90b3ab
sfbtm <dilansanv033@gmail.com>
2026-04-28 00:16:41 -0500

    eventos añadidos para eliminar tareas

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,5 @@
 import { tarjetaTarea } from "./components/tarjetaTarea.js";
-import { get, post } from "./helpers/api.js";
+import { delet, get, post } from "./helpers/api.js";
 
 
 const app = document.querySelector(".app")
@@ -7,6 +7,24 @@ const app = document.querySelector(".app")
 // elementos del HTML que crean una tarea:
 const inputCrear = document.querySelector("#input-crear")
 const botonAgregar = document.querySelector(".form__button")
+
+
+
+
+const listaTareas = document.querySelector(".task-list");
+
+const actualizarLista = async() => {
+    
+    listaTareas.innerHTML = "";
+    const tareasLista = await get("tasks");
+    tareasLista.forEach (async(tarea) => {
+        console.log(tarea)
+        const tarjeta = await tarjetaTarea(tarea)
+        listaTareas.append(tarjeta)
+    })
+}
+actualizarLista()
+
 
 botonAgregar.addEventListener("click", async e => {
     e.preventDefault()
@@ -24,18 +42,16 @@ botonAgregar.addEventListener("click", async e => {
     actualizarLista()
 })
 
+listaTareas.addEventListener("click", async (e) => {
+  const tarjeta = e.target.closest("li");
+  const id = tarjeta?.dataset.id;
 
-const listaTareas = document.querySelector(".task-list");
+  if (e.target.matches(".task-list__button--delete")) {
+    await delet(`tasks/${id}`);
+    tarjeta.remove(); 
+  }
 
-
-const actualizarLista = async() => {
-
-    listaTareas.innerHTML = "";
-    const tareasLista = await get("tasks");
-    tareasLista.forEach (async(tarea) => {
-        console.log(tarea)
-        const tarjeta = await tarjetaTarea(tarea)
-        listaTareas.append(tarjeta)
-    })
-}
-actualizarLista()
+  if (e.target.matches(".task-list__button--edit")) {
+    
+  }
+})
