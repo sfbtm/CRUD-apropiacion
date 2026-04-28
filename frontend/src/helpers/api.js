90d30fee42c58a68b0234552fcb7abc2a8634906
sfbtm <dilansanv033@gmail.com>
2026-04-27 23:18:16 -0500

    peticiones get, post, delete, put llevadas a un helper (api.js)

---
+++ frontend/src/helpers/api.js
@@ -0,0 +1,65 @@
+const url = "http://localhost:3000/"
+
+// crear tareas
+export const post = async(endpoint, data) => {
+    try {
+        const peticion = await fetch (`${url}${endpoint}`,{
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+        if (!peticion.ok) throw new Error('error al enviar tarea');
+    
+        const res = await peticion.json();
+        console.log(res);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+// obtener tareas
+export const get = async(endpoint) => {
+    try {
+        const peticion = await fetch (`${url}${endpoint}`)
+        if (!peticion.ok) throw new Error('error al encontrar tarea');
+    
+        const data = await peticion.json();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// actualizar completamente
+export const put = async(endpoint,data) => {
+    try {
+        const peticion = await fetch (`${url}${endpoint}`,{
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+        if (!peticion.ok) throw new Error('error al enviar tarea');
+    
+        const res = await peticion.json();
+        console.log(res);
+
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export const delet = async (endpoint) => {
+    try {
+
+        const peticion = await fetch (`${url}${endpoint}`,{
+            method: 'DELETE'
+        })
+        if (!peticion.ok) throw new Error('error al enviar tarea');
+    
+        const res = await peticion.json();
+        console.log(res);
+    } catch (err) {
+        console.error(err)
+    }
+}
